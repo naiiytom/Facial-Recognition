70e679c173140db7c59efce3a1d2f8ee500db687
Yuttapichai Lamnaonan <y.lamnaonan@gmail.com>
2018-11-12 15:37:15 +0700

    more changes to come

--- tools/rename.py
+++ tools/rename.py
@@ -11,7 +11,7 @@ for i in range(len(subdir)):
         images = os.listdir(facedir)
         images.sort()
         for j in range(len(images)):
-            print('rename' + images[j] + ' to ===> ' + facedir.split('\\')[1] + '_%03d.jpg' % j)
+            print('rename: ' + images[j] + ' to ===> ' + facedir.split('\\')[1] + '_%03d.jpg' % j)
             os.rename(os.path.join(facedir, images[j]), os.path.join(facedir, (facedir.split('\\')[1] + '_%03d.jpg' % j)))
 
 
