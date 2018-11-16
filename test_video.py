c3a8913e97d93dc5680cb009b356a9b131c0689e
Yuttapichai Lamnaonan <y.lamnaonan@gmail.com>
2018-11-17 04:02:10 +0700

    changes

--- test_video.py
+++ test_video.py
@@ -1,6 +1,3 @@
-from __future__ import absolute_import
-from __future__ import division
-from __future__ import print_function
 
 import os
 import pickle
@@ -13,14 +10,15 @@ from scipy import misc
 
 from packages import facenet, detect_face
 
-input_video="too.mp4"
+input_video="BNK48.MP4"
 modeldir = './models/20180408-102900'
 classifier_filename = './class/classifier.pkl'
 npy='./packages'
-train_img="./BNK"
+train_img="./datasets"
 
 with tf.Graph().as_default():
-    gpu_options = tf.GPUOptions(per_process_gpu_memory_fraction=0.6)
+    #gpu_options = tf.GPUOptions(per_process_gpu_memory_fraction=0.6)
+    gpu_options = tf.GPUOptions(allow_growth=True)
     sess = tf.Session(config=tf.ConfigProto(gpu_options=gpu_options, log_device_placement=False))
     with sess.as_default():
         pnet, rnet, onet = detect_face.create_mtcnn(sess, npy)
@@ -112,7 +110,7 @@ with tf.Graph().as_default():
                         print(best_class_indices,' with accuracy ',best_class_probabilities)
 
                         # print(best_class_probabilities)
-                        if best_class_probabilities>0.053:
+                        if best_class_probabilities>0.25:
                             cv2.rectangle(frame, (bb[i][0], bb[i][1]), (bb[i][2], bb[i][3]), (0, 255, 0), 2)    #boxing face
 
                             #plot result idx under box
